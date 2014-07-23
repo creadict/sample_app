4f14226e63fc5e1e17d6ef42ba47d181b0871812
creadict <creadict@gmail.com>
2014-07-23 10:45:22 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f72f6ca988faa07ce9ec3522373664ffb44e4bfe044b4b5a56685109ca21f79beab38ad916ae61f78a1b61d63f6d6e8ca296959a2360e6c68cca69400a147dbb'
